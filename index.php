9b53916db1a0090afe4332d2a06764f468370a46
Stefan Folkesson <stefan.folkesson@it-gymnasiet.se>
2017-09-20 09:01:00 +0200

    commit message

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+	function loopForEternity() {
+
+		for($i = 1; $i>0; $i++) {
+
+			echo("<script> console.log('Eternity')");
+
+		}
+
+	} 
+
+	function stängdindörr(){
+		for($i=0;$i<10;$i++){
+			echo "Slam!....</br>";
+		}
+		echo "dörren stängs!...";
+	}
+	echo "William älskar PHP!";
+
+	function dödaRobin(){
+		for($i=0; $i<10; $i++){
+			echo "döda Robin... </br>";
+		}
+		echo "Robin	är död";
+	}
+
+	function öppnadindörr(){
+		for($i=0;$i<10;$i++){
+			echo "Dörren öppnas...";
+		}
+	}
+  function familyName($fname, $year) {
+		echo "$fname Refsnes. Born in $year <br>";
+	}
+	
+	familyName("Hege", "1975");
+	familyName("Stale", "1978");
+	familyName("Kai Jim", "1983");
+
+	echo "William älskar PHP!";
+
+	function öppnadindörr(){
+		for($i=0;$i<10;$i++){
+			echo "Knarr..</br>";
+		}
+		echo "Dörren öppnas...";
+	}
+
+	$iphone = "Stefan loves iPhone!";
+	echo $iphone;
+  function slårInDinDörr(){
+		echo "SHABAAAAAEEEEEM";
+
+	}
+?>
